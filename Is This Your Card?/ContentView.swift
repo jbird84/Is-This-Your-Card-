675e5a036f7ed2194791eb3f91f44f86b0776f1c
DoodMac <48062579+jbird84@users.noreply.github.com>
2021-07-27 20:11:29 -0500

    Initial Commit

---
+++ Is This Your Card?/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Is This Your Card?
+//
+//  Created by Kinney Kare on 7/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Is This Your Card?/Is_This_Your_Card_App.swift
@@ -0,0 +1,17 @@
+//
+//  Is_This_Your_Card_App.swift
+//  Is This Your Card?
+//
+//  Created by Kinney Kare on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Is_This_Your_Card_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
